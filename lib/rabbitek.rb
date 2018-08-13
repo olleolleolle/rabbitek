1aa9f3b98e586b7d5485792ec8833338582f7e8a
kruczjak <kruczjak@gmail.com>
2018-08-13 23:28:54 +0200

    Add rubocop

--- lib/rabbitek.rb
+++ lib/rabbitek.rb
@@ -1,5 +1,9 @@
-require "rabbitek/version"
+# frozen_string_literal: true
 
+require 'rabbitek/version'
+
+##
+# High performance background job processing
 module Rabbitek
   # Your code goes here...
 end
